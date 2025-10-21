8e30b47f69b88d11da4e72801f888c5a0d3c9c76
DarkRooftop <1739911721@qq.com>
2025-10-22 00:10:44 +0800

    二叉树查找

--- 数据结构实现/源.cpp
+++ 数据结构实现/源.cpp
@@ -3,77 +3,63 @@
 
 using namespace std;
 
-//数据结构表示二叉树：静态链表
+//二叉搜索树：左边一定小于根节点，右边一定大于根节点
 #define MaxTree 10
-#define ElementType char
-#define Tree int
-#define Null -1
+#define ElementType int
 
-struct TreeNode {
-	ElementType Element;
-	Tree Left;
-	Tree Right;
-}T1[MaxTree], T2[MaxTree];
+typedef struct TreeNode* BinTree;
+typedef BinTree Position;
+struct TreeNode{
+	ElementType Data;
+	BinTree Left;
+	BinTree Right;
+};
 
-Tree BuildTree(struct TreeNode T[]) {
-	int N = 0, check[MaxTree], i = 0;
-	char cl, cr;
-	Tree Root;
-	cin >> N;
-	if (N) {
-		for (i = 0; i < N; i++)check[i] = 0;
-		for (i = 0; i < N; i++) {
-			cin >> T[i].Element >> cl >> cr;
+Position Find(ElementType X, BinTree BST);
+Position Find2(ElementType X, BinTree BST);
+Position FindMin(BinTree BST);
+Position FindMax(BinTree BST);
 
-			if (cl != '-') {
-				T[i].Left = cl - '0';
-				check[T[i].Left] = 1;
-			}
-			else T[i].Left = Null;
-
-			//对cr的处理
-			if (cr != '-') {
-				T[i].Right = cr - '0';
-				check[T[i].Right] = 1;
-			}
-			else T[i].Right = Null;
-		}
-		for (i = 0; i < N; i++)
-			if (!check[i])break;
-
-		Root = i;
-	}
-	return Root;
-}
-
-int isomorphic(Tree R1, Tree R2) {
-	if ((R1 == Null) && (R2 == Null))//都空
-		return 1;
-	if (((R1 == Null) && (R2 != Null)) || ((R2 == Null) && (R1 != Null)))//一个空一个不空
-		return 0;
-	if (T1[R1].Element != T2[R2].Element)//根节点不一样
-		return 0;
-	if ((T1[R1].Left == Null) && (T2[R2].Left == Null))//都没有左儿子
-		return isomorphic(T1[R1].Right, T2[R2].Right);
-	if ((T1[R1].Left != Null) && (T2[R2].Left != Null) &&
-		(T1[T1[R1].Left].Element) == (T2[T2[R2].Left].Element))//不需要交换左右儿子（子树）比较
-		return (isomorphic(T1[R1].Left, T2[R2].Left) &&
-			isomorphic(T1[R1].Right, T2[R2].Right));
-	else
-		return (isomorphic(T1[R1].Left, T2[R2].Right) &&//需要交换
-			isomorphic(T1[R1].Right, T2[R2].Left));
-
-}
+BinTree Insert(ElementType X, BinTree BST);
+BinTree Delete(ElementType X, BinTree BST);
 
 int main() {
-	Tree R1, R2;
-	//建二叉树1
-	R1 = BuildTree(T1);
-	//建二叉树2
-	R2 = BuildTree(T2);
-	//判别是否同构并输出
-	if (isomorphic(R1, R2))printf("Yes\n");
+	
 
 
 	return 0;
+}
+
+Position Find(ElementType X, BinTree BST) {
+	if (!BST) return NULL;//没找到
+	if (X > BST->Data)
+		return Find(X, BST->Right);//大于，右子树找--尾递归
+	else if (X < BST->Data)
+		return Find(X, BST->Left);//小于，左子树找--尾递归--可以用循环实现（迭代函数，执行效率更高）
+	else
+		return BST;//找到了，返回结点地址
+}
+Position Find2(ElementType X, BinTree BST) {
+	while (BST) {
+		if (X > BST->Data)
+			BST = BST->Right;
+		else if (X < BST->Data)
+			BST = BST->Left;
+		else
+			return BST;
+	}
+	return NULL;
+}
+Position FindMin(BinTree BST) {//递归函数
+	if (!BST)return NULL;
+	else if (!BST->Left)
+		return BST;//叶结点返回
+	else
+		return FindMin(BST->Left);//沿左分支继续查找
+}
+Position FindMax(BinTree BST) {//迭代函数
+	if (BST)
+		while (BST->Right)BST = BST->Right;
+	else
+		return BST;
 }
